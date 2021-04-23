5f92b15a6ef64e9f7646aeb5a6eac9f04f5ee6ee
alessandro <alebeni02@gmail.com>
2021-04-23 15:53:01 +0200

    ,

---
+++ dist/main.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    journal:[
+      {
+        titolo: 'Food Corner: Top Japanese Restaurants for Sushi',
+        immagine: './images/single-post-img3.jpg'
+      },
+      {
+        titolo: "Roundup: My New Favourite Recipes For Healthy Living",
+        immagine: './images/fi-roundup-600x395.jpg'
+      },
+      {
+        titolo: "Why These Toasts with Tea are My New Favorite",
+        immagine: './images/fi-toasts-600x395.jpg'
+      }
+    ],
+  },
+  methods:{
+  }
+});
